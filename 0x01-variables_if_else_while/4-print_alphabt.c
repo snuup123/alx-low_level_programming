2f489d036bb9535412a116024f244a931c148aff
snuup123 <dasolidmetal@yahoo.com>
2022-10-17 15:05:05 -0700

    5th

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,16 @@
 *this program prints "programming is positive, zero, or negative
 * Return: 0
 */
-
 int main(void)
-{	char ch;
+{
+	char ch;
 
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 	if (ch != 'e' && ch != 'q')
 {	putchar(ch);
 }
-	putchar('\n');
 }
+	putchar('\n');
 	return (0);
 }
