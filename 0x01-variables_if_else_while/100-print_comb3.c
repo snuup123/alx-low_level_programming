2fafa7431b44b89e2fc087996599f8b893e84a56
snuup123 <dasolidmetal@yahoo.com>
2022-10-17 17:08:58 -0700

    misc

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+*main - prints all possible different combinations of two digits
+*Description: using the main function
+*this program print "01, 02, 03, 04, 05, 06, 07, 08, 09, 12, 13, 14, 15, ...
+* Return: 0
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 9 ; i++)
+{
+	for (j = 1 ; j <= 9 ; j++)
+{
+	if (j > i)
+{
+	putchar(i + '0');
+	putchar(j + '0');
+	if (i != 8)
+{																		putchar(',');
+	putchar(' ');
+}
+}																	}
+}
+	putchar('\n');
+	return (0);
+}
