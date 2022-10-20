e4c04f0409c684070e429b1685d5be98030b2675
snuup123 <dasolidmetal@yahoo.com>
2022-10-20 16:07:37 -0700

    awwh

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* _isupper - checks if it is a upperharcater or not
+* @c: charcater to be tested
+* Return: 1 weather it is , 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+
+{
+	return (1);
+}
+	else
+	return (0);
+}
