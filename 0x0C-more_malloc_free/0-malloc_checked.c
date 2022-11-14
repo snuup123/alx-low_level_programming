380bc720fb14411500f1d6f7d0cde37ef3c74b41
snuup123 <dasolidmetal@yahoo.com>
2022-11-14 14:46:58 +0000

    ok

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -14,4 +14,4 @@ void *malloc_checked(unsigned int b)
         exit(98);                                                                                                                 
                                                                                                                                   
         return (mem);                                                                                                             
-} 
+}
